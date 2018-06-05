bb28117891b36fac02c48bb20d576e364d6ab7c4
Gabriel Felix <crowleyfelix@gmail.com>
2018-06-05 00:14:09 -0300

    Create application entry point

---
+++ src/server/server.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/crowleyfelix/star-wars-api/src/configuration"
+	"github.com/gin-gonic/gin"
+)
+
+type Server interface {
+	Start()
+}
+
+type server struct {
+	engine *gin.Engine
+}
+
+//NewServer creates a web server
+func NewServer() Server {
+	return &server{
+		engine: gin.Default(),
+	}
+}
+
+func (s *server) Start() {
+	s.registerRoutes()
+
+	port := fmt.Sprintf(":%d", configuration.Get().Port)
+
+	err := s.engine.Run(port)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func (s *server) registerRoutes() {
+
+}
