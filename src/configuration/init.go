36f7cbe4398b0901421fc3dffa0f7afe2145c1e9
Gabriel Felix <crowleyfelix@gmail.com>
2018-06-06 01:27:28 -0300

    Creates mongodb configurations

--- src/configuration/init.go
+++ src/configuration/init.go
@@ -1,24 +1,9 @@
 package configuration
 
-import (
-	"os"
-	"strconv"
-)
-
 var (
 	config *Configuration
 )
 
 func init() {
 	load()
-}
-
-func load() {
-	config = new(Configuration)
-	config.Port, _ = strconv.Atoi(os.Getenv("PORT"))
-}
-
-//Get returns application configuration loaded
-func Get() *Configuration {
-	return config
 }
