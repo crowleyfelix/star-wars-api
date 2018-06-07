f29dbb7ce3a9cde38b4be53dc84527152c36ba71
Gabriel Felix <crowleyfelix@gmail.com>
2018-06-07 04:01:55 -0300

    Adds logging

--- src/mongodb/pool.go
+++ src/mongodb/pool.go
@@ -2,6 +2,7 @@ package mongodb
 
 import (
 	"github.com/crowleyfelix/star-wars-api/src/configuration"
+	"github.com/golang/glog"
 	"gopkg.in/mgo.v2"
 )
 
@@ -19,19 +20,29 @@ func (p *pool) Session() (*mgo.Session, error) {
 	var err error
 
 	if p.session == nil {
+		glog.Info("Creating new session")
+
 		uri := configuration.Get().MongoDB.URI
 		p.session, err = mgo.Dial(uri)
 
 		if err != nil {
+			glog.Error("Failed on creating session")
+
 			return nil, err
 		}
 	}
 
 	p.active <- 1
+	glog.Info("%d active sessions", len(p.active))
+
 	return p.session.Copy(), nil
 }
 
 func (p *pool) Release(session *mgo.Session) {
+	glog.Info("Releasing session")
+
 	<-p.active
+
+	glog.Info("%d active sessions", len(p.active))
 	session.Close()
 }
