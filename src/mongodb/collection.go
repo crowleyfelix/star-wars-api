7fb803ebc77dad7693c5e5d57b46a65002e23f49
Gabriel Felix <crowleyfelix@gmail.com>
2018-06-07 02:13:38 -0300

    Creates sequencial id

--- src/mongodb/collection.go
+++ src/mongodb/collection.go
@@ -1,10 +1,17 @@
 package mongodb
 
-import mgo "gopkg.in/mgo.v2"
+import (
+	"errors"
+
+	"github.com/crowleyfelix/star-wars-api/src/mongodb/models"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
 type collection struct {
 	DataBase   string
 	Collection string
+	CounterID  string
 }
 
 func (c *collection) execute(operation func(*mgo.Collection) error) error {
@@ -18,4 +25,32 @@ func (c *collection) execute(operation func(*mgo.Collection) error) error {
 
 	col := session.DB(c.DataBase).C(c.Collection)
 	return operation(col)
+}
+
+func (c *collection) calculateNextID(db *mgo.Database) (int, error) {
+
+	var counter models.Counter
+
+	info, err := db.C("counters").
+		Find(bson.M{
+			"_id": c.CounterID,
+		}).
+		Apply(mgo.Change{
+			Update: bson.M{
+				"$inc": bson.M{
+					"sequence_value": 1,
+				},
+			},
+			ReturnNew: true,
+		}, &counter)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if info.Matched == 0 {
+		return 0, errors.New("failed calculating id sequence")
+	}
+
+	return counter.SequenceValue, nil
 }
