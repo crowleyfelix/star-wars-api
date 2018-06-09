804d1a1f3c1d908a87c90be78ff46fc1edee7c41
Gabriel Felix <crowleyfelix@gmail.com>
2018-06-09 15:27:11 -0300

    Fixes function calls

--- server/main.go
+++ server/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/aphistic/gomol"
+	gc "github.com/aphistic/gomol-console"
+	"github.com/crowleyfelix/star-wars-api/server/configuration"
+	"github.com/crowleyfelix/star-wars-api/server/controllers"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	NewServer().Start()
+	start()
+}
+
+func start() {
+	setUp()
+
+	engine := gin.Default()
+	registerRoutes(engine)
+
+	port := fmt.Sprintf(":%d", configuration.Get().Port)
+
+	err := engine.Run(port)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func setUp() {
+	consoleCfg := gc.NewConsoleLoggerConfig()
+	consoleLogger, _ := gc.NewConsoleLogger(consoleCfg)
+	consoleLogger.SetTemplate(gc.NewTemplateFull())
+
+	gomol.AddLogger(consoleLogger)
+	gomol.InitLoggers()
+
+	ch := make(chan error)
+
+	go func() {
+		for err := range ch {
+			fmt.Printf("[Internal Error] %s\n", err.Error())
+		}
+	}()
+
+	gomol.SetErrorChan(ch)
+}
+
+func registerRoutes(engine *gin.Engine) {
+	controllers.RegisterRoutes(engine.Group("/"))
 }
